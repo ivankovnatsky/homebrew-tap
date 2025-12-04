c0425bd30ec06a6da29708942ff277ce6f62b215
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-12-04 17:19:06 +0200

    comet: update to 142.1.7444.29688

--- Casks/comet.rb
+++ Casks/comet.rb
@@ -1,8 +1,8 @@
 cask "comet" do
-  version "141.0.7390.23964"
+  version "142.1.7444.29688"
   sha256 :no_check
 
-  url "https://www.perplexity.ai/rest/browser/binaries/141.0.7390.23964/comet_latest.dmg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=9d06dad57704bf499ceb71a8730b22e4%2F20251027%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20251027T200354Z&X-Amz-Expires=300&X-Amz-Signature=3a37c7b8689ab20a078b0722083eaa67cbc2d20d51dcb14b33f5f58e394b4339&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject"
+  url "https://pplx-browser-binaries.a0adf9b772aecba4fa8883581f3c9180.r2.cloudflarestorage.com/142.1.7444.29688/comet_latest.dmg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=9d06dad57704bf499ceb71a8730b22e4%2F20251204%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20251204T151732Z&X-Amz-Expires=300&X-Amz-Signature=5eb3f899e06618e484f56088b6469c6deef044f33359f7cc0c08100d5c7b8c4a&X-Amz-SignedHeaders=host&x-amz-checksum-mode=ENABLED&x-id=GetObject"
   name "Comet"
   desc "Comet browser by Perplexity AI"
   homepage "https://www.perplexity.ai"
